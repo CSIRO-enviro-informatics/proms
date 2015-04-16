29c8ff85b302be9aa6f8170146edd2fb4c20e7e3
woo392 <stuart.woodman@csiro.au>
2015-04-16 14:00:48 +1000

    Unnecessary parameter was being set in SPARQLWrapper

--- functions_db.py
+++ functions_db.py
@@ -1,15 +1,12 @@
 import settings
-import requests
 import SPARQLWrapper
 
 
 def db_query(sparql_query):
     sparql = SPARQLWrapper.SPARQLWrapper(settings.FUSEKI_QUERY_URI, returnFormat=SPARQLWrapper.JSON)
-
     sparql.setQuery(sparql_query)
     sparql.method = 'POST'
     #sparql.requestMethod = 'URLENCODED'
-
     try:
         return sparql.query().convert()
     except Exception, e:
@@ -76,7 +73,6 @@ def db_insert_secure(turtle, from_string=False):
         return e.message
 
 
-
 def db_insert_secure_named_graph(turtle, graph_uri, from_string=False):
     #convert the Turtle into N-Triples
     import rdflib
@@ -91,26 +87,11 @@ def db_insert_secure_named_graph(turtle, graph_uri, from_string=False):
     sparql.setCredentials(settings.FUSEKI_SECURE_USR, settings.FUSEKI_SECURE_PWD)
     sparql.setQuery('INSERT DATA { GRAPH ' + graph_uri + ' { ' + g.serialize(format='nt') + ' } }')
     sparql.method = 'POST'
-    if graph_uri:
-        sparql.addParameter("named-graph-uri", graph_uri)
     try:
         return [True, sparql.query()]
     except Exception, e:
         print e.message
         return e.message
-
-
-
-def submit_stardog_query(query):
-    uri = settings.PROMS_DB_URI
-    qsa = {'query': query}
-    h = {'accept': 'application/sparql-results+json'}
-    r = requests.get(uri, params=qsa, headers=h, auth=('proms', 'proms'))
-
-    if r.status_code == 200:
-        return [True, r.text]
-    else:
-        return [False, 'ERROR: ' + r.text]
 
 
 def get_values_from_sparql_results(results, vars):
@@ -120,5 +101,4 @@ def get_values_from_sparql_results(results, vars):
         for var in vars:
             row.append(triple[var]['value'])
         ret.append(row)
-
     return ret
