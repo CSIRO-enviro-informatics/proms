4e43d1e90b718858d5800f982f51d51cd5e4d35c
woo392 <stuart.woodman@csiro.au>
2015-05-20 11:03:36 +1000

    HOME_DIR needed to be lowercase. Made the default (wrong) URIs a bit more generic, and added a comment about the password

--- settings.py
+++ settings.py
@@ -1,18 +1,19 @@
-HOME_DIR = '/opt/PROMS/'
+HOME_DIR = '/opt/proms/'
 STATIC_DIR = 'static/'
 LOGFILE = HOME_DIR + 'proms.log'
 PORT = 9000
 DEBUG = True
 
-#PROMS_INSTANCE_NAMESPACE_URI = 'http://butterfree-bu.nexus.csiro.au/proms/'
-PROMS_INSTANCE_NAMESPACE_URI = 'http://localhost:9000/'
+PROMS_INSTANCE_NAMESPACE_URI = 'http://localhost/'
 
-FUSEKI_QUERY_URI = 'http://115.146.94.255:3030/data/query'
-FUSEKI_UPDATE_URI = 'http://115.146.94.255:3030/data/update'
+FUSEKI_QUERY_URI = 'http://localhost/fuseki/data/query'
+FUSEKI_UPDATE_URI = 'http://localhost/fuseki/data/update'
 
-FUSEKI_SECURE_QUERY_URI = 'http://115.146.94.255/fuseki/data/query'
-FUSEKI_SECURE_UPDATE_URI = 'http://115.146.94.255/fuseki/data/update'
+FUSEKI_SECURE_QUERY_URI = 'http://localhost/fuseki/data/query'
+FUSEKI_SECURE_UPDATE_URI = 'http://localhost/fuseki/data/update'
+
 FUSEKI_SECURE_USR = 'fusekiusr'
+# Make sure this password matches the one entered during Fuseki installation
 FUSEKI_SECURE_PWD = 'fusekirocks'
 # Request Timeout in seconds
 FUSEKI_TIMEOUT = 5
