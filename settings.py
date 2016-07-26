e6a50e59a46126a837111539a4c4a9d8b5b7a2d2
Nicholas Car <nicholas.car@csiro.au>
2016-07-26 22:47:33 +1000

    better settings defaults

--- settings.py
+++ settings.py
@@ -1,13 +1,12 @@
 import sys
 
-VERSION = '3.0.1'
 SECRET_KEY = 'hello, proms'
 
-ENTITY_BASE_URI = 'http://localhost/entity'
-ACTIVITY_BASE_URI = "http://localhost/activity"
-AGENT_BASE_URI = "http://localhost/agent"
-REPORT_BASE_URI = "http://localhost/report"
-REPORTINGSYSTEM_BASE_URI = "http://localhost/reportingsystem"
+ENTITY_BASE_URI = 'http://localhost/id/entity'
+ACTIVITY_BASE_URI = "http://localhost/id/activity"
+AGENT_BASE_URI = "http://localhost/id/agent"
+REPORT_BASE_URI = "http://localhost/id/report"
+REPORTINGSYSTEM_BASE_URI = "http://localhost/id/reportingsystem"
 
 WEB_SUBFOLDER = ''
 
@@ -19,7 +18,7 @@ if 'dev' in sys.argv or '--dev' in sys.argv:
     HOST = '0.0.0.0'
     PORT = 9000
     DEBUG = True
-    MONGODB = "127.0.0.1"
+    MONGODB = '127.0.0.1'
 
     PROMS_INSTANCE_NAMESPACE_URI = 'http://localhost:9000/'
 
@@ -46,7 +45,7 @@ if 'dev' in sys.argv or '--dev' in sys.argv:
 
 else:
     PROMS_LABEL = 'A PROMS Server'
-    HOME_DIR = '/home/django/BA/proms/'
+    HOME_DIR = 'c:/work/proms/'
     STATIC_DIR = 'static/'
     LOGFILE = HOME_DIR + 'proms.log'
     HOST = '0.0.0.0'
@@ -55,13 +54,13 @@ else:
     MONGODB = "127.0.0.1"
 
     #PROMS_INSTANCE_NAMESPACE_URI = 'http://localhost/'
-    PROMS_INSTANCE_NAMESPACE_URI = 'http://proms-dev1-vc.it.csiro.au/'
+    PROMS_INSTANCE_NAMESPACE_URI = 'http://localhost:9000' + WEB_SUBFOLDER
 
-    FUSEKI_QUERY_URI = 'http://localhost/fuseki/data/query'
-    FUSEKI_UPDATE_URI = 'http://localhost/fuseki/data/update'
+    FUSEKI_QUERY_URI = 'http://localhost:3030/data/query'
+    FUSEKI_UPDATE_URI = 'http://localhost:3030/data/update'
 
-    FUSEKI_SECURE_QUERY_URI = 'http://localhost/fuseki/data/query'
-    FUSEKI_SECURE_UPDATE_URI = 'http://localhost/fuseki/data/update'
+    FUSEKI_SECURE_QUERY_URI = 'http://localhost:3030/data/query'
+    FUSEKI_SECURE_UPDATE_URI = 'http://localhost:3030/data/update'
 
     FUSEKI_SECURE_USR = 'fusekiusr'
     # Make sure this password matches the one entered during Fuseki installation
@@ -76,5 +75,5 @@ else:
     # Comma separated list of known PROMS instances that receive pingback
     KNOWN_PROMS_INSTANCES = []
 
-    DPN_BASE_URI = 'http://sample-dpn-instance.com'
+    DPN_BASE_URI = 'http://localhost'
 
