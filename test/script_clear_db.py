fdf3199f8f63010ea71dcba7ea4fd4e423b997f3
Nicholas Car <nicholas.car@csiro.au>
2016-08-08 23:28:45 +1000

    clears named graph and defualt graph

--- test/script_clear_db.py
+++ test/script_clear_db.py
@@ -1,12 +1,17 @@
 import requests
 import imp
-settings = imp.load_source('', '/opt/proms/settings.py')
+settings = imp.load_source('', 'c:/work/proms/settings.py')
 
 
 def db_delete_all_secure():
     """ Make a secure delete of all triples in the DB
     """
-    # SPARQL DELETE
+    # SPARQL  all named graphs
+    data = {'update': 'DELETE WHERE { GRAPH ?g {?s ?p ?o}}'}
+    auth = (settings.FUSEKI_SECURE_USR, settings.FUSEKI_SECURE_PWD)
+    r = requests.post(settings.FUSEKI_SECURE_UPDATE_URI, data=data, auth=auth)
+
+    # SPARQL DELETE all non-named graphs (default graph)
     data = {'update': 'DELETE WHERE {?s ?p ?o}'}
     auth = (settings.FUSEKI_SECURE_USR, settings.FUSEKI_SECURE_PWD)
     r = requests.post(settings.FUSEKI_SECURE_UPDATE_URI, data=data, auth=auth)
