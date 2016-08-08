349e7cb5cec728df6b564c439e1090cfa70ca99a
Nicholas Car <nicholas.car@csiro.au>
2016-08-09 02:34:58 +1000

    a start at handling incoming pingbacks

---
+++ pingbacks/handle_incoming/test/script_post_prov_aq_pingback.py
@@ -0,0 +1,25 @@
+import requests
+import settings
+
+
+def post_static_prov_aq_pingback():
+    headers = {}
+    headers['Content-Type'] = 'text/uri-list'
+    headers['Content-Length'] = '0'
+    link_header1 = '<http://entity.com/1>; ' + \
+                  'rel="http://www.w3.org/ns/prov#has_provenance; ' + \
+                  'anchor="http://somewhere-else.com"'
+    link_header2 = '<http://entity.com/1>; ' + \
+                  'rel="http://www.w3.org/ns/prov#has_provenance; ' + \
+                  'anchor="http://somewhere-else-else.com"'
+    headers['Link'] = link_header1 + ', ' + link_header2
+
+    send_to = settings.PROMS_INSTANCE_NAMESPACE_URI + '/function/pingback'
+    print send_to
+    r = requests.post(send_to,
+                      data=None,
+                      headers=headers)
+
+
+    print r.status_code
+    print r.content
