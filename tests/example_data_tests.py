176545d33be15127aabf6b418d46c44cfd1d5708
Nicholas Car <nicholas.car@ga.gov.au>
2017-01-05 23:54:54 +1100

    using statically declared files

--- tests/example_data_tests.py
+++ tests/example_data_tests.py
@@ -1,7 +1,9 @@
 import os
 import requests
-import settings
 import database  # must have PROMS Server & DB running
+
+PROMS_BASE_URI = 'http://localhost:9000'
+PROMS_HOME_DIR = 'c:/work/proms/'
 
 
 def purge_db():
@@ -12,8 +14,8 @@ def purge_db():
 def load_rs():
     # load the example ReportingSystem, System 01
     r = requests.post(
-        settings.BASE_URI + '/function/lodge-reportingsystem',
-        data=open(os.path.join(settings.HOME_DIR, 'tests', 'example_data_rs_01.ttl')).read(),
+        PROMS_BASE_URI + '/function/lodge-reportingsystem',
+        data=open(os.path.join(PROMS_HOME_DIR, 'tests', 'example_data_rs_01.ttl')).read(),
         headers={'Content-Type': 'text/turtle'}
     )
     print r.content
@@ -23,32 +25,32 @@ def load_rs():
 def load_reports():
     # load 4 Reports for System 01
     r = requests.post(
-        settings.BASE_URI + '/function/lodge-report',
-        data=open(os.path.join(settings.HOME_DIR, 'tests', 'example_data_rs_01_report_01.ttl')).read(),
+        PROMS_BASE_URI + '/function/lodge-report',
+        data=open(os.path.join(PROMS_HOME_DIR, 'tests', 'example_data_rs_01_report_01.ttl')).read(),
         headers={'Content-Type': 'text/turtle'}
     )
     print r.content
     assert r.status_code == 201
 
     r = requests.post(
-        settings.BASE_URI + '/function/lodge-report',
-        data=open(os.path.join(settings.HOME_DIR, 'tests', 'example_data_rs_01_report_02.ttl')).read(),
+        PROMS_BASE_URI + '/function/lodge-report',
+        data=open(os.path.join(PROMS_HOME_DIR, 'tests', 'example_data_rs_01_report_02.ttl')).read(),
         headers={'Content-Type': 'text/turtle'}
     )
     print r.content
     assert r.status_code == 201
 
     r = requests.post(
-        settings.BASE_URI + '/function/lodge-report',
-        data=open(os.path.join(settings.HOME_DIR, 'tests', 'example_data_rs_01_report_03.ttl')).read(),
+        PROMS_BASE_URI + '/function/lodge-report',
+        data=open(os.path.join(PROMS_HOME_DIR, 'tests', 'example_data_rs_01_report_03.ttl')).read(),
         headers={'Content-Type': 'text/turtle'}
     )
     print r.content
     assert r.status_code == 201
 
     r = requests.post(
-        settings.BASE_URI + '/function/lodge-report',
-        data=open(os.path.join(settings.HOME_DIR, 'tests', 'example_data_rs_01_report_04.ttl')).read(),
+        PROMS_BASE_URI + '/function/lodge-report',
+        data=open(os.path.join(PROMS_HOME_DIR, 'tests', 'example_data_rs_01_report_04.ttl')).read(),
         headers={'Content-Type': 'text/turtle'}
     )
     print r.content
@@ -58,23 +60,25 @@ def load_reports():
 def test_example_data_rs_html():
     # get the HTML
     # get URI
-    get_uri = '%(BASE_URI)s/register?_uri=%(quoted_uri)s' % {
-                  'BASE_URI': settings.BASE_URI,
-                  'quoted_uri': 'http%3A%2F%2Fpromsns.org%2Fdef%2Fproms%23ReportingSystem'
-              }
+    get_uri = '%(PROMS_BASE_URI)s/register?_uri=%(quoted_uri)s' % {
+        'PROMS_BASE_URI': PROMS_BASE_URI,
+        'quoted_uri': 'http%3A%2F%2Fpromsns.org%2Fdef%2Fproms%23ReportingSystem'
+    }
     r = requests.get(get_uri)
     html = r.content
-    line_to_seek = '<li><a href="/instance?_uri=%(quoted_uri)s">System 01</a></li>' % \
-                   {'quoted_uri': 'http%3A%2F%2Fpid.geoscience.gov.au%2Fsystem%2Fsystem-01'}
+    line_to_seek = '<li><a href="%(PROMS_BASE_URI)s/instance?_uri=%(quoted_uri)s">System 01</a></li>' % {
+        'PROMS_BASE_URI': PROMS_BASE_URI,
+        'quoted_uri': 'http%3A%2F%2Fpid.geoscience.gov.au%2Fsystem%2Fsystem-01'
+    }
     assert line_to_seek in html
 
 
 def test_example_data_rs_rdf():
     # get the RDF in turtle
-    get_uri = '%(BASE_URI)s/register?_uri=%(quoted_uri)s&_format=text/turtle' % {
-                  'BASE_URI': settings.BASE_URI,
-                  'quoted_uri': 'http%3A%2F%2Fpromsns.org%2Fdef%2Fproms%23ReportingSystem'
-              }
+    get_uri = '%(PROMS_BASE_URI)s/register?_uri=%(quoted_uri)s&_format=text/turtle' % {
+        'PROMS_BASE_URI': PROMS_BASE_URI,
+        'quoted_uri': 'http%3A%2F%2Fpromsns.org%2Fdef%2Fproms%23ReportingSystem'
+    }
     r = requests.get(get_uri)
     html = r.content
     # fragile tests as using formatted RDF, not logical RDF
